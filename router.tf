906d6105e7792efa4947672dc654c2d70abc75ac
Daniel Omokha <danielomokha04@gmail.com>
2024-08-20 13:53:04 +0100

    Delete router.tf

--- router.tf
+++
@@ -1,7 +0,0 @@
-# https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/compute_router
-resource "google_compute_router" "router" {
-  name    = "router"
-  project = "supple-defender-349711"
-  region  = "us-central1"
-  network = google_compute_network.main.id
-}
